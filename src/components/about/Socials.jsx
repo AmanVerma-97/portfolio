4b5849aadf61e9c0b6c9a03877bbd94c502dcf78
Aman Verma <amanksverma@gmail.com>
2025-03-05 11:55:57 +0530

    change socials

--- src/components/about/Socials.jsx
+++ src/components/about/Socials.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react'
 const socials=[
     {src:"https://cdn-icons-png.flaticon.com/128/25/25657.png", alt:"github", link:"http://github.com/AmanVerma-97", name:"Github"},
     {src:"https://cdn-icons-png.flaticon.com/128/3536/3536505.png", alt:"linkedin", link:"http://linkedin.com/in/amanv97", name:"LinkedIn"},
-    {src:"https://cdn-icons-png.flaticon.com/128/5968/5968830.png", alt:"twitter", link:"https://twitter.com/AmanVerma_97", name:"Twitter"},
     {src:"https://img.icons8.com/?size=80&id=9L16NypUzu38&format=png", alt:"leetcode", link:"https://leetcode.com/aman_verma_19/", name:"LeetCode"},
 ]
 
