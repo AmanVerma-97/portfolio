cd72542f2287d6fc34f092d8ebd4887c1c2913a1
Aman Verma <amanksverma@gmail.com>
2025-07-27 18:05:53 +0530

    Add Gen AI Project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -7,7 +7,7 @@ function Projects() {
   return (
     <>
     
-      <div className=' w-10/12 md:w-11/12 lg:w-10/12 m-auto grid grid-cols-2 md:grid-cols-3 place-content-center place-items-center gap-4 mt-2 md:mt-8 p-2'>
+      <div className=' w-10/12 md:w-11/12 lg:w-10/12 m-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 place-content-center place-items-center gap-4 mt-2 md:mt-8 p-2'>
 
           {
             projects.map((project, index)=>{
@@ -16,18 +16,18 @@ function Projects() {
                 <a href={project.link} target='blank'>
                   
                   <div className=' flex flex-col items-center'  key={index}>
-                    <div className=' lg:w-52 lg:h-52 md:w-40 md:h-40 h-36 w-36  flex justify-center items-center rounded-xl shadow-md shadow-black hover:shadow-lg hover:shadow-orange-400 origin-center'
+                    <div className=' lg:w-52 lg:h-52 md:w-44 md:h-44 h-36 w-36  flex justify-center items-center rounded-xl shadow-md shadow-black hover:shadow-lg hover:shadow-orange-400 origin-center'
                          onMouseOver={()=>sethoverIndex(index)} onMouseOut={()=>sethoverIndex(null)}>
                       {hoverIndex===index ? 
                       <div className='w-full h-full bg-black/90 flex flex-col justify-center items-center rounded-xl'>
-                        <h1 className=' font-bold text-lg text-white drop-shadow-lg'>Tech Stack</h1>
+                        <h1 className=' font-bold text-lg text-white drop-shadow-lg mb-1 underline'>Tech Stack</h1>
                         <p className=' font-semibold text-sm lg:text-base text-gray-100 drop-shadow-lg'> {techStack[index].skill1} </p>
                         <p className=' font-semibold text-sm lg:text-base text-gray-100 drop-shadow-lg'> {techStack[index].skill2} </p>
                         <p className=' font-semibold text-sm lg:text-base text-gray-100 drop-shadow-lg'> {techStack[index].skill3} </p>
                       </div> 
                       
                       :
-                      <img src={project.img} alt={project.name} role='img'  className='w-full h-full bg-contain rounded-xl text-white border '/>
+                      <img src={project.img} alt={project.name} role='img'  className='w-full h-full bg-contain rounded-xl text-white border'/>
                       }
                     </div>
                     <h2 className=' text-lg font-bold'>{project.name}</h2>
